a380a6bd2e38a523135372214769fbb5f6e0581f
20172104257 <39482044+20172104257@users.noreply.github.com>
2018-06-05 15:39:40 +0800

    Initial commit

---
+++ jichengpaishenglei/jichengpaishenglei.cpp
@@ -0,0 +1,75 @@
+// jichengpaishenglei.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+class CFeet
+{
+protected:
+	int feet;
+	int inches;
+public:
+	CFeet();
+	CFeet(int th, int fh);
+	~CFeet();
+	void display();
+	int getfeet();
+	int getinche();
+};
+CFeet::CFeet()
+{
+	feet = 0;
+	inches = 0;
+	cout << "基类默认构造函数" << feet << "英尺" << inches << "英寸" << endl;
+}
+CFeet::CFeet(int th, int fh)
+{
+	feet = th;
+	inches = fh;
+	cout<<"基类构造函数" << feet << "英尺" << inches << "英寸" << endl;
+}
+CFeet::~CFeet()
+{
+	cout << "基类析构函数" << feet << "英尺" << inches << "英寸" << endl;
+}
+void CFeet::display()
+{
+	cout<<"Display" << feet << "英尺" << inches << "英寸" << endl;
+}
+int CFeet::getfeet()
+{
+	return feet;
+}
+int CFeet::getinche()
+{
+	return inches;
+}
+class CMyfeet :public CFeet
+{
+public:
+	CMyfeet();
+	CMyfeet(int th, int fh);
+	~CMyfeet();
+};
+CMyfeet::CMyfeet()
+{
+	cout<<"派生类默认构造函数"<<feet << "英尺" << inches << "英寸" << endl;
+}
+CMyfeet::CMyfeet(int t, int f)//:CFeet(t,f)
+{
+	feet = t;
+	inches = f;
+	cout<< "派生类构造函数" << feet << "英尺" << inches << "英寸" << endl;
+}
+CMyfeet::~CMyfeet()
+{
+	cout<< "派生类析构函数" << feet << "英尺" << inches << "英寸" << endl;
+}
+int main()
+{
+	CMyfeet of1(2,6);
+	of1.display();
+    return 0;
+}
+
